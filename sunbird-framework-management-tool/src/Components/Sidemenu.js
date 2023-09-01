047421ae28547b59af7de8161728fc3d54aca82e
katakampranavi <pranavi123katakam@gmail.com>
2023-09-01 19:53:05 +0530

    removed inline style elements for side menu

--- sunbird-framework-management-tool/src/Components/Sidemenu.js
+++ sunbird-framework-management-tool/src/Components/Sidemenu.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import { Drawer, Accordion, AccordionSummary, AccordionDetails, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import styles from '../Styles/styles.module.css';
 
-const drawerWidth = 250; // Set your desired width here
+const drawerWidth = 250;
 
-const buttonStyle = {
-  marginBottom: '1rem',
-  width: '100%',
-  backgroundColor: '#3b5998',
-  color: 'white',
-  transition: 'background-color 0.3s',
-  textTransform: 'none',
-  fontSize: '17px',
+const menuSections = [
+  {
+    title: 'Create',
+    links: [
+      { text: 'Framework', to: '/framework/create' },
+      { text: 'Category', to: '/category/create' },
+      { text: 'Terms', to: '/term/create' },
+    ],
+  },
+  {
+    title: 'List',
+    links: [
+      { text: 'Framework', to: '/framework/list' },
+      { text: 'Category', to: '/category/list' },
+      { text: 'Terms', to: '/term/list' },
+    ],
+  },
+];
+const headingStyle = {
   fontFamily: 'gothvetica',
+  fontSize: '26px',
+  color: 'white',
 };
-
-const accordionSummaryStyle = {
-  backgroundColor: '#3b5998',
-};
-
-const accordionDetailsStyle = {
-  backgroundColor: '#3b5998',
-};
-
 function SideMenu({ handleCreateClick, handleListClick }) {
   return (
     <Drawer
@@ -31,85 +36,33 @@ function SideMenu({ handleCreateClick, handleListClick }) {
       variant="permanent"
       PaperProps={{ style: { width: drawerWidth, backgroundColor: '#D8CFD0', border: '1px inset black' } }}
     >
-      <div>
-        <Accordion expanded={true} onChange={handleCreateClick}>
-          <AccordionSummary style={accordionSummaryStyle}>
-            <Typography style={{ color: 'white', fontFamily: 'gothvetica', fontSize: 26 }}>
-              <strong>Create</strong>
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails style={accordionDetailsStyle}>
-            <Link to="/framework/create">
-              <Button
-                style={{ ...buttonStyle }}
-                onMouseEnter={(e) => (e.target.style.backgroundColor = '#004080')} // Hover effect
-                onMouseLeave={(e) => (e.target.style.backgroundColor = buttonStyle.backgroundColor)} // Remove hover effect
-              >
-                Framework
-              </Button>
-            </Link>
-            <Link to="/category/create">
-              <Button
-                style={{ ...buttonStyle }}
-                onMouseEnter={(e) => (e.target.style.backgroundColor = '#004080')} // Hover effect
-                onMouseLeave={(e) => (e.target.style.backgroundColor = buttonStyle.backgroundColor)} // Remove hover effect
-              >
-                Category
-              </Button>
-            </Link>
-            <Link to="/term/create">
-              <Button
-                style={{ ...buttonStyle }}
-                onMouseEnter={(e) => (e.target.style.backgroundColor = '#004080')} // Hover effect
-                onMouseLeave={(e) => (e.target.style.backgroundColor = buttonStyle.backgroundColor)} // Remove hover effect
-              >
-                Terms
-              </Button>
-            </Link>
-          </AccordionDetails>
-        </Accordion>
-      </div>
-      <div>
-        <Accordion expanded={true} onChange={handleListClick}>
-          <AccordionSummary style={accordionSummaryStyle}>
-            <Typography style={{ color: 'white', fontFamily: 'gothvetica', fontSize: 26 }}>
-              <strong>List</strong>
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails style={accordionDetailsStyle}>
-            <Button
-              style={{ ...buttonStyle }}
-              onMouseEnter={(e) => (e.target.style.backgroundColor = '#004080')} // Hover effect
-              onMouseLeave={(e) => (e.target.style.backgroundColor = buttonStyle.backgroundColor)} // Remove hover effect
-            >
-              <Link to="/framework/list" style={{ color: ' white', textDecoration: 'none' }}>
-                Framework
-              </Link>
-            </Button>
-            <Button
-              style={{ ...buttonStyle }}
-              onMouseEnter={(e) => (e.target.style.backgroundColor = '#004080')} // Hover effect
-              onMouseLeave={(e) => (e.target.style.backgroundColor = buttonStyle.backgroundColor)} // Remove hover effect
-            >
-              <Link to="/category/list" style={{ color: 'white', textDecoration: 'none' }}>
-                Category
-              </Link>
-            </Button>
-            <Button
-              style={{ ...buttonStyle }}
-              onMouseEnter={(e) => (e.target.style.backgroundColor = '#004080')} // Hover effect
-              onMouseLeave={(e) => (e.target.style.backgroundColor = buttonStyle.backgroundColor)} // Remove hover effect
-            >
-              <Link to="/term/list" style={{ color: 'white', textDecoration: 'none' }}>
-                Terms
-              </Link>
-            </Button>
-          </AccordionDetails>
-        </Accordion>
-      </div>
-      <div style={{ backgroundColor: '#3b5998', height: '100%', border: 'none' }}></div>
+      {menuSections.map((section, index) => (
+        <div key={index}>
+          <Accordion expanded={true} onChange={section.title === 'List' ? handleListClick : handleCreateClick}>
+            <AccordionSummary className={styles.accordionSummaryStyle}>
+              <Typography className={styles.createTitle} style={headingStyle}>
+                <strong>{section.title}</strong>
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails className={styles.accordionDetailsStyle}>
+              {section.links.map((link, linkIndex) => (
+                <Button className={styles.buttonStyle}>
+                  <Link to={link.to} className={styles.linkStyle}>
+                    {link.text}
+                  </Link>
+                </Button>
+              ))}
+            </AccordionDetails>
+          </Accordion>
+        </div>
+      ))}
+      <div className={styles.additionalStyle}></div>
     </Drawer>
   );
 }
 
 export default SideMenu;
+
+
+
+
